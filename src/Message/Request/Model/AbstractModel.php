6befa35ce061f898e57bb5a7d6d173142f838c0b
Tetiuev Pavel <jetexe2@gmail.com>
2020-01-31 09:57:03 +0500

    Remove old

--- src/Message/Request/Model/AbstractModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types = 1);
-
-namespace AvtoDev\CloudPayments\Message\Request\Model;
-
-abstract class AbstractModel implements ModelInterface
-{
-}
--- src/Message/Strategy/Exception/StrategyCannotCreateResponseException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types = 1);
-
-namespace AvtoDev\CloudPayments\Message\Strategy\Exception;
-
-class StrategyCannotCreateResponseException extends \InvalidArgumentException
-{
-}
